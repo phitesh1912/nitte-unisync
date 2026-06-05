53c6927f8b9b625de3cb126ec84e1eeefb0537e4
phitesh1912 <1nt23cb041.punati@nmit.ac.in>
2026-06-05 17:08:01 +0530

    fix authDomain

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const env = import.meta.env || {};
 
 const firebaseConfig = {
   apiKey:            env.VITE_FIREBASE_API_KEY || "AIzaSyBK45OsFP1gu3VEpUPFbcFzwZoZv0fvzyw",
-  git authDomain:        "nitte-unisync.vercel.app",
+ authDomain:        "nitte-unisync.vercel.app",
   projectId:         env.VITE_FIREBASE_PROJECT_ID || "nitte-connect-b6913",
   storageBucket:     env.VITE_FIREBASE_STORAGE_BUCKET || "nitte-connect-b6913.firebasestorage.app",
   messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID || "53232594967",
